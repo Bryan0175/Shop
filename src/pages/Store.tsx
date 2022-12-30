afa4a0dde5d6240731e764c20ad89f5cbdc42143
Bryan0175 <bryanskate0175@hotmail.com>
2022-12-30 04:09:13 -0500

    all

---
+++ src/pages/Store.tsx
@@ -0,0 +1,33 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import storeItems from "../data/items.json"
+import {useMemo, useState} from "react";
+
+export function Store() {
+
+    const [text, setText] = useState("");
+
+    const filteredServices = useMemo(() => {
+        return storeItems.filter(item => (
+            item.name.toLowerCase().includes(text.toLowerCase())
+        ))
+    },[storeItems, text]);
+
+  return (
+    <>
+      <h1>Tienda</h1>
+        <input placeholder="¿Qué servicio buscas?"
+               type="text"
+               value={text}
+               onChange={(e) => setText(e.target.value)}
+               autoFocus={true}/>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {filteredServices.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
