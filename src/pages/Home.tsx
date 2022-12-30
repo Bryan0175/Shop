37a19807a5288d38b91cec6ef4c5afc9a9fbf773
Bryan0175 <bryanskate0175@hotmail.com>
2022-12-30 04:46:39 -0500

    size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export function Home() {
   return (
       <div>
         <h1>Home</h1>
-          <Image src={bg} width={1300} height={600} alt="background" title="background" style={{marginTop: 50,marginBottom: 50}}/>
+          <Image src={bg} width={800} height={600} alt="background" title="background" style={{marginTop: 50,marginBottom: 50}}/>
         <p><strong>ROPAYA</strong>, es un emprendimiento que aspira llegar a todos los corazones. Tenemos una amplia variedad de productos con una excelente calidad y lo mejor ¡Son realizados 100% a mano!
           Puedes adquirir productos, para ti, para regalar o incluso para tu mascota.
           No dudes en comprar la amplia variedad de productos con una excelente calidad y lo mejor ¡Son realizados 100% a mano!
